99ee761a986dff086ff743d17032a33b018bb6fd
beardgod7 <nwaokefrancis@gmail.com>
2025-06-19 14:15:34 -0700

    changeer

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,8 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../config/vtb-notifications-firebase-adminsdk-fbsvc-c329129b80.json");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../config/vtb-notifications-firebase-adminsdk-fbsvc-c329129b80.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-module.exports = admin;
+// module.exports = admin;
