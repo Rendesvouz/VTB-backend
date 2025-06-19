e221d860de22ec89dfab527a4c025ed656c7c574
beardgod7 <nwaokefrancis@gmail.com>
2025-06-19 15:05:27 -0700

    changeer

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 
 // Allowed frontend origins
 const allowedOrigins = [
+  "*",
   "http://localhost:3000",
   //"https://your-production-frontend.com", // ✅ Replace with actual frontend production URL
 ];
